5a8c688989a9aa7b0f7cf8dcab5de8725b743b96
DISHA MEHTA <disha.mehta1410@gmail.com>
2025-09-20 11:10:05 +0530

    Add files via upload

---
+++ PYTHON/BUILT-IN-FUNCTION/builtin.py
@@ -0,0 +1,80 @@
+
+
+# built in function
+
+# using print we can log the value as output
+
+print("Hello!!!")
+
+# using input functionwe can take input from users
+
+input("Enter something:/")
+
+# type 
+
+# using type operator we can get the datatype of our varicable 
+
+num=20
+
+print(type(num))
+
+# typecasting
+# using typecasting function we can change the data types
+
+num="6"
+
+print(type(num))
+
+int(num)
+
+print(type(num))
+
+# str
+
+num=10
+
+print(type(num))
+
+print(str(num))
+
+print(type(num))
+
+# float
+
+num=8
+
+print(type(num))
+
+print(float(num))
+
+print(type(num))
+
+word = "python"
+
+print(len(word))
+
+# sum
+
+numbers=[1,2,3,4,5,6,7]
+
+print(sum(numbers))
+
+# sort
+
+print(sorted(numbers))
+
+# decending
+
+print(sorted(numbers,reverse=True))
+
+# max
+
+scores=[10,40,50,60,20]
+
+print(max(scores))
+
+print(min(scores))
+
+for i in range(1,15):
+    print(i)
+
